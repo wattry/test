ff0d4b4c724ad7c2da204f4c8e04a686688ec8a7
Ryan <09wattry@gmail.com>
2024-02-04 19:30:00 -0800

    Test pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://htmlpreview.github.io/?https://github.com/rockstarphil/rockstarphil.com/blob/docs/dist/',
+  base: '/',
   plugins: [react()],
 })
